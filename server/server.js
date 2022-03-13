495bd1a3380f1f2aa0a63e2ffbd44e724f15cba3
Davey-M <davey.meuer@gmail.com>
2022-03-12 21:10:16 -0600

    add socket io to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
 
 const port = 5000 || process.env.PORT;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log('App running on port:', port));
+// app.get('/test', (req, res) => {
+//   res.sendStatus(200);
+// })
+
+io.on('connection', (socket) => {
+  console.log(socket);
+})
+
+// app.listen(port, () => console.log('App running on port:', port));
+httpServer.listen(port);
--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,9 +2,16 @@ import './App.css';
 import io from 'socket.io-client';
 
 import Player from '../Player/Player';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const socket = io();
+  const socket = io('http://localhost:5000');
+
+  // useEffect(async () => {
+  //   let response = await axios.get('/test');
+  //   console.log(response);
+  // });
 
   console.log(socket);
   return (
